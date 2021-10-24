2509b37e6b6f64596cb55ad8099c20e6edbbd3ec
miloKnell <miloknell@gmail.com>
2021-10-24 13:12:46 -0700

    Update analysis.py

--- Milo/analysis.py
+++ Milo/analysis.py
@@ -46,7 +46,7 @@ def get_day_pred(df):
     gb= df.groupby('days')
     day_n = gb.apply(lambda x: len(x))
     day = gb['days'].apply(lambda x: x.name)
-    date = gb['date']
+    date = gb['date'].apply(lambda x: x.iloc[0].date())
     day_overrides = gb.apply(lambda x: (x['first_place'] != x['winner']).sum()) / day_n
     day_pred= gb.apply(lambda x: x['pred'].sum()) / day_n
     day_clicks = gb.apply(lambda x: x['click_rate'].sum())/ day_n
@@ -57,12 +57,7 @@ day, day_pred, day_clicks = get_day_pred(df)
 override_day, override_pred, override_clicks = get_day_pred(overrides)
 no_override_day, no_override_pred, no_override_clicks = get_day_pred(no_overrides)
 
-'''plt.plot(override_day, savgol_filter(override_pred, 41, 3), label='overrides')
-plt.plot(no_override_day, savgol_filter(no_override_pred, 41, 3), label='no overrides')
-'''
 
-'''plt.plot(override_day, override_pred, label='overrides')
-plt.plot(no_override_day, no_override_pred, label='no overrides')'''
 
 
 '''
@@ -90,7 +85,7 @@ views['days'] = delta.apply(lambda x: x.days)
 
 
 
-w=[(col, spearmanr(df[col], df['click_rate'])) for col in df.loc[:, 'quotes':'MoralityGeneral']]
+w=[(col, spearmanr(df[col], df['pred'])) for col in df.loc[:, 'quotes':'MoralityGeneral']]
 s=sorted(w, key=lambda x: abs(x[1][0]))
 
 #for q in s:
@@ -101,5 +96,51 @@ s=sorted(w, key=lambda x: abs(x[1][0]))
 
 
 '''
-fig 3.2.2.1: smoothed days vs date
+#fig 3.2.2.1: date vs smoothed click_rate
+color1 = 'tab:blue'
+color2 = 'tab:red'
+
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Daily average click rate')
+ax1.plot(day,savgol_filter(day_clicks, 41, 3), color=color1, label='average click rate')
+ax1.tick_params(axis='y', labelcolor=color1)
+
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('Daily percent headlines predicted to be fake news')
+ax2.plot(day, savgol_filter(day_pred, 41, 3), color=color2, label='percent fake')
+ax2.tick_params(axis='y', labelcolor=color2)
+
+plt.title('Comparison of Daily Click Rate and Daily Percent Fake News')
+
+plt.show()
+
+'''
+
+'''
+analysis 3.2.2
+corr = spearmanr(df['click_rate'], df['pred'])
+'''
+
+
+
+
+'''
+#fig 3.3.x.1
+
+plt.plot(override_day, savgol_filter(override_pred, 41, 3), label='Overrides')
+plt.plot(no_override_day, savgol_filter(no_override_pred, 41, 3), label='No overrides')
+
+plt.xlabel('Date')
+plt.ylabel('Daily Percent Clickbaits (predicted fake news)')
+plt.legend()
+plt.show()
+'''
+
+'''
+#fig 3.3.x.2
+
+plt.bar(day, override_pred-no_override_pred)
+plt.show()
 '''
